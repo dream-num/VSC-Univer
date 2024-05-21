cb61584986e613321b4433f4b637423f336cc2df
karl <2557075675@qq.com>
2024-05-21 14:23:44 +0800

    feat: add the basic sheets and docs function into vscode

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,92 @@
 import * as vscode from 'vscode'
+import { Sheets } from './univer/sheets/main'
 
 export function activate(context: vscode.ExtensionContext) {
-  const disposable = vscode.commands.registerCommand('vs-univer.Univer', () => {
-    vscode.window.showInformationMessage('Hello Univer!')
-  })
+  context.subscriptions.push(
+    vscode.commands.registerCommand('vs-univer.Sheets', () => {
+      const panel = vscode.window.createWebviewPanel(
+        'univer-sheets',
+        'univer-sheets',
+        vscode.ViewColumn.One,
+        {
+          enableScripts: true,
+          localResourceRoots: [vscode.Uri.file(context.extensionPath)],
+        },
+      )
 
-  context.subscriptions.push(disposable)
+      panel.webview.html = getWebviewContent(context, panel, 'sheets')
+    }),
+  )
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand('vs-univer.Docs', () => {
+      const panel = vscode.window.createWebviewPanel(
+        'univer-docs',
+        'univer-docs',
+        vscode.ViewColumn.One,
+        {
+          enableScripts: true,
+          localResourceRoots: [vscode.Uri.file(context.extensionPath)],
+        },
+      )
+
+      panel.webview.html = getWebviewContent(context, panel, 'docs')
+    }),
+  )
+}
+
+function getWebviewContent(context: vscode.ExtensionContext, panel: vscode.WebviewPanel, type: 'sheets' | 'docs') {
+  const targetFile = type === 'sheets' ? 'sheets' : 'docs'
+  const webview = panel.webview
+  const mainCssPath = vscode.Uri.file(
+    context.asAbsolutePath(`media/${targetFile}/main.css`),
+  )
+
+  const mainJsPath = vscode.Uri.file(
+    context.asAbsolutePath(`media/${targetFile}/main.js`),
+  )
+
+  const faviconPath = vscode.Uri.file(
+    context.asAbsolutePath('media/favicon.svg'),
+  )
+
+  const mainCssUri = webview.asWebviewUri(mainCssPath)
+  const mainJsUri = webview.asWebviewUri(mainJsPath)
+  const faviconUri = webview.asWebviewUri(faviconPath)
+
+  return `
+  <!doctype html>
+    <html lang="en">
+        <head>
+            <meta charset="utf-8" />
+            <meta content="width=device-width, initial-scale=1" name="viewport" />
+            <title>Univer Sheets</title>
+
+            <link rel="icon" type="image/x-icon" href="${faviconUri}" />
+            <link rel="stylesheet" href="${mainCssUri}" />
+            <style>
+                html,
+                body {
+                    height: 100%;
+                    margin: 0;
+                }
+            </style>
+
+            <script>
+                new EventSource('/esbuild').addEventListener('change', () => {
+                    console.info('reload--');
+                    location.reload();
+                });
+            </script>
+        </head>
+
+        <body style="overflow: hidden">
+            <div id="app" style="height: 100%"></div>
+
+            <script src="${mainJsUri}"></script>
+        </body>
+    </html>
+  `
 }
 
 export function deactivate() {}
