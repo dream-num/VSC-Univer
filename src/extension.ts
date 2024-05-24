4877189f03756f8b45f1184f4d8606759b97ff53
karl <2557075675@qq.com>
2024-05-24 17:04:10 +0800

    feat: make the base feature of vs-univer

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,6 @@
 import * as fs from 'node:fs'
 import * as path from 'node:path'
 import * as vscode from 'vscode'
-import * as xlsx from 'xlsx'
 
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
@@ -45,44 +44,6 @@ export function activate(context: vscode.ExtensionContext) {
       panel.webview.html = getWebviewContent(context, panel, 'docs')
 
       setupMessageListener(panel)
-    }),
-  )
-
-  context.subscriptions.push(
-    vscode.commands.registerCommand('vs-univer.LoadExcel', async () => {
-      const options: vscode.OpenDialogOptions = {
-        canSelectMany: false,
-        openLabel: 'Open',
-        filters: {
-          'Excel Files': ['xlsx'],
-        },
-      }
-
-      const fileUri = await vscode.window.showOpenDialog(options)
-      if (fileUri && fileUri[0]) {
-        try {
-          const buffer = fs.readFileSync(fileUri[0].fsPath)
-          const workbook = xlsx.read(buffer, { type: 'buffer' })
-          const sheetName = workbook.SheetNames[0]
-          const sheet = workbook.Sheets[sheetName]
-          const data = xlsx.utils.sheet_to_json(sheet, { header: 1 })
-
-          const panel = vscode.window.createWebviewPanel(
-            'univerExcel',
-            'univer-excel',
-            vscode.ViewColumn.One,
-            {
-              enableScripts: true,
-              // localResourceRoots: [vscode.Uri.file(context.extensionPath)],
-            },
-          )
-
-          panel.webview.html = getWebviewXlsx(data)
-        }
-        catch (error) {
-          vscode.window.showErrorMessage('Failed to load excel file!')
-        }
-      }
     }),
   )
 }
@@ -167,21 +128,6 @@ function getWebviewContent(context: vscode.ExtensionContext, panel: vscode.Webvi
         </script>
         </body>
     </html>
-  `
-}
-
-function getWebviewXlsx(data: any) {
-  return `
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-      <meta charset="utf-8">
-      <title>Excel Data</title>
-  </head>
-  <body>
-      <pre>${JSON.stringify(data, null, 2)}</pre>
-  </body>
-  </html>
   `
 }
 
